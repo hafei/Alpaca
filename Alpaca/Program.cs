69664d0291bd51b4147caa730489a63bb57ca939
hafei <zhangmengcoding@gmail.com>
2018-06-18 00:34:56 +0800

    comments

--- Alpaca/Program.cs
+++ Alpaca/Program.cs
@@ -14,7 +14,9 @@ namespace Alpaca
     {
         public static void Main(string[] args)
         {
+            //
             CreateWebHostBuilder(args).Build().Run();
+            new WebHostBuilder();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
--- Alpaca/Startup.cs
+++ Alpaca/Startup.cs
@@ -9,9 +9,14 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace Alpaca
 {
+    /// <summary>
+    /// 定义请求处理管道
+    /// 配置应用需要的服务
+    /// </summary>
     public class Startup
     {
         public Startup(IConfiguration configuration)
@@ -19,8 +24,13 @@ namespace Alpaca
             Configuration = configuration;
         }
 
+        //配置信息
         public IConfiguration Configuration { get; }
 
+        /// <summary>
+        /// 定义应用所使用的服务
+        /// </summary>
+        /// <param name="services"></param>
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
@@ -35,9 +45,16 @@ namespace Alpaca
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
+        /// <summary>
+        /// 定义请求管道中的中间件
+        /// </summary>
+        /// <param name="app"></param>
+        /// <param name="env"></param>
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env ,ILoggerFactory loggerFactory)
         {
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
